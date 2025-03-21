f63feff9c7c48f78a5c68bd5e489fee414854e21
carlosraziel <carlosrazielsanchezromero@gmail.com>
2025-03-20 21:35:36 -0600

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -2,7 +2,7 @@ let Titulo = document.title;
 
 window.addEventListener('blur', () => {
     Titulo = document.title;
-    document.title = "Para ti amiguita :)";
+    document.title = "Para ti amiguita linda :)";
 })
 
 window.addEventListener('focus', () => {
